45a7d21d9192e1a52f4e6462ca0741478fcb6ad7
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-25 15:12:51 +0300

    02_ConcatenateNames

---
+++ 4.Data Types and Variables - Lab/02_ConcatenateNames.js
@@ -0,0 +1,6 @@
+function concatenate(name, lastName, symbol) {
+    console.log(name + symbol + lastName);
+}
+concatenate('John', 'Smith', '->')
+concatenate('Jan', 'White', '<->')
+concatenate('Linda', 'Terry', '=>')
