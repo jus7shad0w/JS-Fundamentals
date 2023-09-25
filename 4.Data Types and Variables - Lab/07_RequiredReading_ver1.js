6edd98577db830f46fe9735412b18c7da1523f30
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-09-25 16:37:15 +0300

    07_RequiredReading_ver1

---
+++ 4.Data Types and Variables - Lab/07_RequiredReading_ver1.js
@@ -0,0 +1,5 @@
+function reading(book, pages, days) {
+    console.log((book / pages) / days);
+}
+reading(212, 20, 2)
+reading(432, 15, 4)
---
+++ 4.Data Types and Variables - Lab/07_RequiredReading_ver2.js
@@ -0,0 +1,7 @@
+function reading(book, pages, days) {
+    let timeToFinish = book / pages;
+    let HoursPerDay = timeToFinish / days;
+    console.log(HoursPerDay);
+}
+reading(212, 20, 2)
+reading(432, 15, 4)
