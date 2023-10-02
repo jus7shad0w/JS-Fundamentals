df0e83bdc27ec38feab7dfa476123ffe500e2df5
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-10-02 19:24:22 +0300

    02_AddAndRemove.js

---
+++ 9.Arrays - More Exercise/02_AddAndRemove.js
@@ -0,0 +1,22 @@
+function addRemove(arr) {
+    let newArr = [];
+    let num = 1;
+
+    for (let command of arr) {
+        if (command === 'add') {
+            newArr.push(num);
+        } else if (command === 'remove') {
+            if (newArr.length > 0) {
+                newArr.pop();
+            }
+        }
+        num++;
+    }
+    if (newArr.length === 0) {
+        console.log('Empty');
+    } else {
+        console.log(newArr.join(' '));
+    }
+}
+addRemove(['add', 'add', 'add', 'add'])
+addRemove(['add', 'add', 'remove', 'add', 'add'])
