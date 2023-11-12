a36f578020fb56076196ff47ed29ef177a1f0822
Pavlin Vasilev <shadow_psv@yahoo.com>
2023-11-12 21:25:35 +0200

    02_CharsToString.js

---
+++ 5.Data Types and Variables - Exercise/02_CharsТoString.js
@@ -0,0 +1,7 @@
+function charToString(char1, char2, char3) {
+    let word = char1 + char2 + char3;
+    console.log(word);
+}
+charToString('a', 'b', 'c')
+charToString('%', '2', 'o')
+charToString('1', '5', 'p')
